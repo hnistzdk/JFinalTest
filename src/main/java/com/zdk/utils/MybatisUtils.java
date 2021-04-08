3036d6fa5631fd24973a4194f77dd4f6070803fb
hnistzdk <369365576@qq.com>
2021-04-08 17:14:53 +0800

    4.8

--- src/main/java/com/zdk/utils/MybatisUtils.java
+++ src/main/java/com/zdk/utils/MybatisUtils.java
@@ -13,7 +13,6 @@ import java.io.InputStream;
  * @date 2021/3/28 15:19
  */
 
-//sqlSessionFactory-->sqlSession
 public class MybatisUtils {
     private static SqlSessionFactory sqlSessionFactory;
     static {
@@ -26,9 +25,6 @@ public class MybatisUtils {
             e.printStackTrace();
         }
     }
-
-    //既然有了 SqlSessionFactory，顾名思义，我们可以从中获得 SqlSession 的实例。
-    // SqlSession 提供了在数据库执行 SQL 命令所需的所有方法。你可以通过 SqlSession 实例来直接执行已映射的 SQL 语句
     public static SqlSession getSqlSession(){
         return sqlSessionFactory.openSession();
     }
